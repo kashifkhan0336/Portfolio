12401ef1643feaa2ae0bbbfcb4a8ae01c63657fd
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2022-07-17 14:28:59 +0500

    contact form in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,76 @@
-import React from 'react'
-import { HiMenuAlt3 } from "react-icons/hi";
-import { NavLink, Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Twirl as Hamburger } from "hamburger-react";
 
 export default function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  console.log(menuOpen)
+
   return (
-    <div className='navbar-container'>
-        <nav className="navbar">
-            <div className="navbar__brand">
-                <NavLink to="/" className="brand">
-                    My Portfolio
-                </NavLink>
-            </div>
-            <ul className="navbar__links">
-                <NavLink to="/" className={({isActive})=>isActive?"navbar__links--link active":"navbar__links--link"}>Home</NavLink>
-                <NavLink to="/about" className={({isActive})=>isActive?"navbar__links--link active":"navbar__links--link"}>About</NavLink>
-                <NavLink to="/skills" className={({isActive})=>isActive?"navbar__links--link active":"navbar__links--link"}>Skills</NavLink>
-                <NavLink to="/projects" className={({isActive})=>isActive?"navbar__links--link active":"navbar__links--link"}>Projects</NavLink>
-            </ul>
-            <Link to="/contact" className="navbar__contact-btn">
-                <div  className="contact-btn">
-                    <span className='contact-btn--skew-fix'>Contact</span>
-                </div>
-            </Link>
-            <div className='navbar__hamburger'>
-                <HiMenuAlt3 className='hamburger' />
-            </div>
-        </nav>
+    <div className="navbar-container">
+      <nav className="navbar">
+        <div className="navbar__brand">
+          <NavLink to="/" className="brand">
+            MA
+          </NavLink>
+        </div>
+        <ul className={menuOpen ? "navbar__links menu-open" : "navbar__links"}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "navbar__links--link active" : "navbar__links--link"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "navbar__links--link active" : "navbar__links--link"
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/skills"
+            className={({ isActive }) =>
+              isActive ? "navbar__links--link active" : "navbar__links--link"
+            }
+          >
+            Skills
+          </NavLink>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              isActive ? "navbar__links--link active" : "navbar__links--link"
+            }
+          >
+            Projects
+          </NavLink>
+        </ul>
+        <Link to="/contact" className={menuOpen ? "navbar__contact-btn btn-open" : "navbar__contact-btn"}>
+          <div className="contact-btn">
+            <span className="contact-btn--skew-fix">Contact</span>
+          </div>
+        </Link>
+        <div className="navbar__hamburger">
+          <Hamburger
+          size={28}
+            toggled={isOpen}
+            toggle={setOpen}
+            color="#FFB700"
+            onToggle={(toggled) => {
+              if (toggled) {
+                setMenuOpen(prevState => !prevState)
+              } else {
+                setMenuOpen(prevState => !prevState)
+              }
+            }}
+          />
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Form from '../components/Form'
 
 export default function Contact() {
+  useEffect(()=>{
+    document.title = "FrontEnd Portfolio - Contact"
+  }, [])
   return (
-    <div className='text'>Contact</div>
+    <section className='form-section'>
+      <Form />
+    </section>
   )
 }
