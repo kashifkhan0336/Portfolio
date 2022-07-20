5470fcddd5e81749b9404af473165027217c5568
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2022-07-20 09:10:55 +0500

    final test

--- src/projects.js
+++ src/projects.js
@@ -1,18 +1,18 @@
 import p1 from "./assets/projects/p1.png";
 import p2 from "./assets/projects/p.png";
-import p3 from "./assets/projects/p3.png";
-import p4 from "./assets/projects/p4.png";
-import p5 from "./assets/projects/p5.png";
-import p6 from "./assets/projects/p6.png";
-import p7 from "./assets/projects/p7.png";
-import p8 from "./assets/projects/p8.png";
-import p9 from "./assets/projects/p9.png";
-import p10 from "./assets/projects/p10.png";
-import p11 from "./assets/projects/p11.png";
-import p12 from "./assets/projects/p12.png";
-import p13 from "./assets/projects/p13.png";
-import p14 from "./assets/projects/p14.png";
-import p15 from "./assets/projects/p15.png";
+import a from "./assets/projects/a.png";
+import b from "./assets/projects/b.png";
+import c from "./assets/projects/c.png";
+import d from "./assets/projects/d.png";
+import e from "./assets/projects/e.png";
+import f from "./assets/projects/f.png";
+import g from "./assets/projects/g.png";
+import h from "./assets/projects/h.png";
+import i from "./assets/projects/i.png";
+import j from "./assets/projects/j.png";
+import k from "./assets/projects/k.png";
+import l from "./assets/projects/l.png";
+import m from "./assets/projects/m.png";
 
 export default [
   {
@@ -21,25 +21,25 @@ export default [
     githubLink: "https://github.com/Drago78624/AttackOnCode-Forum",
   },
   {
-    projectImg: p6,
+    projectImg: d,
     projectTitle: "Tailwind - Microsoft Clone",
     githubLink: "https://github.com/Drago78624/Microsoft-Clone-TailwindCSS",
     liveSite: "https://tw-microsoft-clone.netlify.app/",
   },
   {
-    projectImg: p13,
+    projectImg: k,
     projectTitle: "Patrona Puppy",
     githubLink: "https://github.com/Drago78624/Patrona-Puppy-Final",
     liveSite: "https://62d5805a70d912060199e23e--scintillating-elf-dee3c0.netlify.app/",
   },
   {
-      projectImg: p11,
+      projectImg: i,
       projectTitle: "Bootstrap - KU site",
       githubLink: "https://github.com/Drago78624/KU-2.0",
       liveSite: "https://dreamy-snyder-167b78.netlify.app/",
     },
     {
-      projectImg: p9,
+      projectImg: g,
       projectTitle: "Huddle Landing Page",
       githubLink: "https://github.com/Drago78624/FEM-huddle-landing-page",
       liveSite: "https://compassionate-hypatia-0abd83.netlify.app/",
@@ -51,53 +51,53 @@ export default [
     liveSite: "https://react-todoos.netlify.app/",
   },
   {
-    projectImg: p3,
+    projectImg: a,
     projectTitle: "React - Quote Generator",
     githubLink: "https://github.com/Drago78624/React-QuoteGenerator",
     liveSite: "https://react-qg.netlify.app/",
   },
   {
-    projectImg: p4,
+    projectImg: b,
     projectTitle: "Javascript - Filter",
     githubLink: "https://github.com/Drago78624/Filter-Project",
     liveSite: "https://unrivaled-croissant-096803.netlify.app/",
   },
   {
-    projectImg: p5,
+    projectImg: c,
     projectTitle: "Tailwind - Tic Tac Toe",
     githubLink: "https://github.com/Drago78624/Tic-Tac-Toe",
     liveSite: "https://tw-tictactoe.netlify.app/",
   },
   {
-    projectImg: p7,
+    projectImg: e,
     projectTitle: "Javscript - Quote Generator",
     githubLink: "https://github.com/Drago78624/Quote-Generator-App",
     liveSite: "https://animeqg.netlify.app/",
   },
   {
-    projectImg: p8,
+    projectImg: f,
     projectTitle: "Tailwind - Quiz App",
     githubLink: "https://github.com/Drago78624/Quiz-App",
     liveSite: "https://tw-quizapp.netlify.app/",
   },
   {
-    projectImg: p10,
+    projectImg: h,
     projectTitle: "Javscript - Vote Machine",
     githubLink: "https://github.com/Drago78624/voting-machine",
     liveSite: "https://votemachine.netlify.app/",
   },
   {
-    projectImg: p12,
+    projectImg: j,
     projectTitle: "Javscript - Digital Clock",
     githubLink: "https://github.com/Drago78624/DIgital-Clock"
   },
   {
-    projectImg: p14,
+    projectImg: l,
     projectTitle: "PHP - Todoos",
     githubLink: "https://github.com/Drago78624/ToDoos"
   },
   {
-    projectImg: p15,
+    projectImg: m,
     projectTitle: "ReactChat - Login Page",
     githubLink: "https://github.com/Drago78624/ReactChat-loginPage-responsive"
   },
